969b6cce93fd7ec13dfa094a70c5a3f6eb57e0c9
Sasmita Ojha <sasmitaojha@Sasmitas-MacBook-Pro.local>
2024-01-14 23:52:36 +0530

    refactor conditional check

--- get-log-base2-values.js
+++ get-log-base2-values.js
@@ -1,13 +1,10 @@
 const inputArgs = [2, 4, 6, 8, 9, 15];
 
 const getLogBase2Values = (inputArr) => {
-  if (inputArr?.length > 0) {
-    const filteredItems = inputArr.filter((item) =>
-      Number.isInteger(Math.log2(item))
-    );
-    return filteredItems.map((item) => Math.pow(item, 2));
-  }
-  return inputArr;
+  const filteredItems = inputArr?.filter((item) =>
+    Number.isInteger(Math.log2(item))
+  );
+  return filteredItems?.map((item) => Math.pow(item, 2));
 };
 
 console.log(getLogBase2Values(inputArgs));
